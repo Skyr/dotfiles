e7ff81d9c32ec6e54ec4163a1cdf98fc18a1335e
Stefan Schlott <stefan@ploing.de>
2023-02-06 08:58:36 +0100

    Bump Nerdfont version

--- run_once_after_10_download_fonts.sh
+++ run_once_after_10_download_fonts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if command -v Xorg >/dev/null && [ `id -u` -ne 0 ] ; then
-  NERD_FONT_VERSION="v2.1.0"
+  NERD_FONT_VERSION="v2.3.3"
   NERD_FONT_PATH=$HOME/.local/share/chezmoi/nerd-fonts/$NERD_FONT_VERSION
   mkdir -p $NERD_FONT_PATH
   for fontname in DejaVuSansMono DroidSansMono SourceCodePro Terminus LiberationMono FiraCode FiraMono RobotoMono JetBrainsMono ; do
