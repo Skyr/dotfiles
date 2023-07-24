3c75b73fd968a880eb9ded858b489e2751904f30
Stefan Schlott <stefan@ploing.de>
2023-07-24 12:31:32 +0200

    Config for Treesitter

---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "java",
+        "kotlin",
+        "rust",
+        "scala",
+      },
+    },
+  },
+}
