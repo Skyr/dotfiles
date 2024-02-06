e53da49800629b9b0c4127ed6d37914af477a6d7
Stefan Schlott <stefan@ploing.de>
2024-02-06 23:17:32 +0100

    Remove SpaceVim

--- run_once_before_20_spacevim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if command -v vim > /dev/null ; then
-  if [[ "${DISPLAY:-}" == "" ]] ; then
-    curl -sLf https://spacevim.org/install.sh | bash /dev/stdin --no-fonts
-  else
-    curl -sLf https://spacevim.org/install.sh | bash
-  fi
-fi
