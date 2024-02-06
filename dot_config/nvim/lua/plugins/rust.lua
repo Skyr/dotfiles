bea8cd3f84e8f2669edabf22ecd1c3e62679f089
Stefan Schlott <stefan@ploing.de>
2024-02-06 21:41:27 +0100

    Rust: dap integration

--- dot_config/nvim/lua/plugins/rust.lua
+++ dot_config/nvim/lua/plugins/rust.lua
@@ -1,7 +1,3 @@
---local on_attach = require("plugins.config.lspconfig").on_attach
---local capabilities = require("plugins.config.lspconfig").capabilities
-local util = require("lspconfig/util")
-
 return {
   {
     "williamboman/mason.nvim",
@@ -22,31 +18,67 @@ return {
   },
   {
     "mfussenegger/nvim-dap",
-    opts = {},
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    keys = {
+    dependencies = {
       {
-        "<leader>du",
-        function()
-          require("dapui").toggle({})
+        "rcarriga/nvim-dap-ui",
+        -- stylua: ignore
+        keys = {
+          { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI", },
+          { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = { "n", "v" }, },
+        },
+        opts = {},
+        config = function(_, opts)
+          local dap = require("dap")
+          local dapui = require("dapui")
+          dapui.setup(opts)
+          dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open({})
+          end
+          dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close({})
+          end
+          dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close({})
+          end
         end,
-        desc = "Dap UI",
       },
       {
-        "<leader>de",
-        function()
-          require("dapui").eval()
-        end,
-        desc = "Eval",
-        mode = { "n", "v" },
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {},
       },
-      -- <leader>dt :DapToggleBreakpoint<CR>
-      -- <leader>dx :DapTerminate<CR>
-      -- <leader>do :DapStepOver<CR>
     },
-    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+      { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+      { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+      { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+      { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+      { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+      { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+      { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+      { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+      { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+    },
+    config = function()
+      local Config = require("lazyvim.config")
+      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+
+      for name, sign in pairs(Config.icons.dap) do
+        sign = type(sign) == "table" and sign or { sign }
+        vim.fn.sign_define(
+          "Dap" .. name,
+          { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+        )
+      end
+    end,
   },
   {
     "mrcjkb/rustaceanvim",
@@ -69,7 +101,7 @@ return {
         else
           liblldb_path = extension_path .. "lldb/lib/liblldb.so"
         end
-        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+        adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb_path, liblldb_path)
       end
       return {
         dap = {
@@ -91,6 +123,7 @@ return {
         },
       }
     end,
+    config = function() end,
   },
   {
     "folke/which-key.nvim",
