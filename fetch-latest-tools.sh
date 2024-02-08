0451b65fba26d98678b439ca19bfde6d2b06ce28
Stefan Schlott <stefan@ploing.de>
2024-02-08 09:04:11 +0100

    Add dive to tools

--- fetch-latest-tools.sh
+++ fetch-latest-tools.sh
@@ -14,12 +14,23 @@ function dl_tar() {
 	rm "$TARFILE"
 }
 
+
 dl_tar "https://github.com/Wilfred/difftastic/releases/latest/download/difft-x86_64-unknown-linux-gnu.tar.gz" 0 difft
+
 dl_tar "https://github.com/simonwhitaker/gibo/releases/latest/download/gibo_Linux_arm64.tar.gz" 0 gibo
+
 dl_tar "https://github.com/extrawurst/gitui/releases/latest/download/gitui-linux-musl.tar.gz" 0 ./gitui 
+
 LAZYGIT_URL=$(curl --silent "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | jq -r ".assets[].browser_download_url" | grep "Linux_x86_64")
 dl_tar "$LAZYGIT_URL" 0 lazygit
+
 RGA_URL=$(curl --silent "https://api.github.com/repos/phiresky/ripgrep-all/releases/latest" | jq -r ".assets[].browser_download_url" | grep "x86_64-unknown-linux-musl")
+
 dl_tar "$RGA_URL" 1 '*/rga'
+
 curl -L -s -o $HOME/.local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
 chmod +x $HOME/.local/bin/yq
+
+DIVE_URL=$(curl --silent "https://api.github.com/repos/wagoodman/dive/releases/latest" | jq -r ".assets[].browser_download_url" | grep "_linux_amd64.tar.gz")
+dl_tar "$DIVE_URL" 0 dive
+
