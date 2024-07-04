021b977cfc32cc1dd58fd2f92f07a4f4432aa499
Stefan Schlott <stefan@ploing.de>
2024-07-04 21:21:37 +0200

    gibo now in fetch-latest-tools

--- chezmoi-imports.sh
+++ chezmoi-imports.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-curl -s -L -o ~/.local/bin/gibo https://raw.githubusercontent.com/simonwhitaker/gibo/master/gibo
-chmod +x ~/.local/bin/gibo
-chezmoi add ~/.local/bin/gibo
-~/.local/bin/gibo update
-
 curl -s -L -o ~/.local/bin/fasd https://github.com/clvv/fasd/raw/master/fasd
 chmod +x ~/.local/bin/fasd
 chezmoi add ~/.local/bin/fasd
 
 mkdir -p ${HOME}/.oh-my-zsh-custom/powerlevel
 chezmoi add ${HOME}/.oh-my-zsh-custom/powerlevel
-curl -s -L -o powerlevel10k-master.tar.gz  https://github.com/romkatv/powerlevel10k/archive/master.tar.gz
+curl -s -L -o powerlevel10k-master.tar.gz https://github.com/romkatv/powerlevel10k/archive/master.tar.gz
 chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh-custom/powerlevel powerlevel10k-master.tar.gz
 rm powerlevel10k-master.tar.gz
 
 mkdir -p ${HOME}/.oh-my-zsh-custom/plugins/fzf-tab
 chezmoi add ${HOME}/.oh-my-zsh-custom/plugins/fzf-tab
-curl -s -L -o fzf-tab-master.tar.gz  https://github.com/Aloxaf/fzf-tab/archive/master.tar.gz
+curl -s -L -o fzf-tab-master.tar.gz https://github.com/Aloxaf/fzf-tab/archive/master.tar.gz
 chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh-custom/plugins/fzf-tab fzf-tab-master.tar.gz
 rm fzf-tab-master.tar.gz
 
@@ -38,4 +33,3 @@ chezmoi add ${HOME}/.oh-my-zsh
 curl -s -L -o oh-my-zsh-master.tar.gz https://github.com/robbyrussell/oh-my-zsh/archive/master.tar.gz
 chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh oh-my-zsh-master.tar.gz
 rm oh-my-zsh-master.tar.gz
-
