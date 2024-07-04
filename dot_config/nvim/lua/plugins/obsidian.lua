3490f9a208fc4be9285dd70d2926f55be408f3b9
Stefan Schlott <stefan@ploing.de>
2024-07-04 14:38:37 +0200

    nvim obsidian

---
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,31 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+  --   "BufReadPre path/to/my-vault/**.md",
+  --   "BufNewFile path/to/my-vault/**.md",
+  -- },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "Rollenspiel",
+        path = "~/Documents/privat/rollenspiel/Rollenspiel.obsidian",
+      },
+    },
+    completion = {
+      nvim_cmp = true,
+      min_chars = 3,
+    },
+  },
+}
