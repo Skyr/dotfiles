4e039e4b56f9e113dadc1075f76ee99d438df70b
Stefan Schlott <stefan@ploing.de>
2024-07-06 17:34:50 +0200

    nvim keymaps for buffers

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+map("n", "<A-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<A-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next Buffer" })
