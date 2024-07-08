5468bd0c63b888c549922676d919f54ea9b7adf9
Stefan Schlott <stefan@ploing.de>
2024-07-08 17:42:12 +0200

    Remove dirs before importing

--- chezmoi-imports.sh
+++ chezmoi-imports.sh
@@ -7,31 +7,31 @@ chezmoi add ~/.local/bin/fasd
 mkdir -p ${HOME}/.oh-my-zsh-custom/powerlevel
 chezmoi add ${HOME}/.oh-my-zsh-custom/powerlevel
 curl -s -L -o powerlevel10k-master.tar.gz https://github.com/romkatv/powerlevel10k/archive/master.tar.gz
-chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh-custom/powerlevel powerlevel10k-master.tar.gz
+chezmoi import -r --strip-components 1 --destination ${HOME}/.oh-my-zsh-custom/powerlevel powerlevel10k-master.tar.gz
 rm powerlevel10k-master.tar.gz
 
 mkdir -p ${HOME}/.oh-my-zsh-custom/plugins/fzf-tab
 chezmoi add ${HOME}/.oh-my-zsh-custom/plugins/fzf-tab
 curl -s -L -o fzf-tab-master.tar.gz https://github.com/Aloxaf/fzf-tab/archive/master.tar.gz
-chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh-custom/plugins/fzf-tab fzf-tab-master.tar.gz
+chezmoi import -r --strip-components 1 --destination ${HOME}/.oh-my-zsh-custom/plugins/fzf-tab fzf-tab-master.tar.gz
 rm fzf-tab-master.tar.gz
 
 mkdir -p ${HOME}/.oh-my-zsh-custom/plugins/zsh-syntax-highlighting
 chezmoi add ${HOME}/.oh-my-zsh-custom/plugins/zsh-syntax-highlighting
 curl -s -L -o zsh-syntax-highlighting-master.tar.gz https://github.com/zsh-users/zsh-syntax-highlighting/archive/refs/heads/master.tar.gz
-chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh-custom/plugins/zsh-syntax-highlighting zsh-syntax-highlighting-master.tar.gz
+chezmoi import -r --strip-components 1 --destination ${HOME}/.oh-my-zsh-custom/plugins/zsh-syntax-highlighting zsh-syntax-highlighting-master.tar.gz
 rm zsh-syntax-highlighting-master.tar.gz
 
 mkdir -p ${HOME}/.oh-my-zsh-custom/plugins/zsh-autosuggestions
 chezmoi add ${HOME}/.oh-my-zsh-custom/plugins/zsh-autosuggestions
 curl -s -L -o zsh-autosuggestions-master.tar.gz https://github.com/zsh-users/zsh-autosuggestions/archive/refs/heads/master.tar.gz
-chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh-custom/plugins/zsh-autosuggestions zsh-autosuggestions-master.tar.gz
+chezmoi import -r --strip-components 1 --destination ${HOME}/.oh-my-zsh-custom/plugins/zsh-autosuggestions zsh-autosuggestions-master.tar.gz
 rm zsh-autosuggestions-master.tar.gz
 
 mkdir -p ${HOME}/.oh-my-zsh
 chezmoi add ${HOME}/.oh-my-zsh
 curl -s -L -o oh-my-zsh-master.tar.gz https://github.com/robbyrussell/oh-my-zsh/archive/master.tar.gz
-chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh oh-my-zsh-master.tar.gz
+chezmoi import -r --strip-components 1 --destination ${HOME}/.oh-my-zsh oh-my-zsh-master.tar.gz
 rm oh-my-zsh-master.tar.gz
 
 mkdir -p ${HOME}/.tmux/plugins
