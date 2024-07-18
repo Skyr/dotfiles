55daea9eef15c9fddc569b499f21c2f45c052733
Stefan Schlott <stefan@ploing.de>
2024-07-18 17:24:41 +0200

    Add neogit

---
+++ dot_config/nvim/lua/plugins/git.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = true,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+  },
+}
