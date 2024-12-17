9943fd8881fd585b5551e38ba7e16c241150131c
Stefan Schlott <stefan@ploing.de>
2024-12-17 22:56:03 +0100

    neovim gitsigns: Stage hunk shortcuts

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -10,5 +10,15 @@ return {
   },
   {
     "lewis6991/gitsigns.nvim",
+    keys = function()
+      local gitsigns = require("gitsigns")
+      return {
+        { "<leader>hs", gitsigns.stage_hunk, desc = "Stage hunk" },
+        { "<leader>hr", gitsigns.reset_hunk, desc = "Reset hunk" },
+        { "<leader>hS", gitsigns.stage_buffer, desc = "Stage buffer" },
+        { "<leader>hu", gitsigns.undo_stage_hunk, desc = "Undo stage hunk" },
+        { "<leader>hR", gitsigns.reset_buffer, desc = "Reset buffer" },
+      }
+    end,
   },
 }
